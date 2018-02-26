4f98b853bdce3c4b8000a0d74af6d5f8ae89ea3a
colbybanbury <cbanbury@udel.edu>
2018-02-26 12:31:37 -0500

    intRead

--- sim.py
+++ sim.py
@@ -16,14 +16,26 @@
 #sub	11	00	00		00
 #		op dest	reg1	reg2
 
+reg0, reg1, reg2, reg3 = 0
+
+regs = [reg0, reg1, reg2, reg3]
+
 fileName = raw_input("enter the name of the file you would like to simulate")
 
 file = open(fileName)
+instructions = file.readlines()						#list of each line in the txt
+instructions = [x.strip("\n") for x in content] 	#therefore a list of the instructions
 
-reg0, reg1, reg2, reg3 = "0000" #store values as strings
+for instruction in instructions
+	opCode = Instruction[:2]
+	
 
-regs = [reg0, reg1, reg2, reg3]
 
 def getReg(regCode):					#converts a binary string to an int 
 	return regs[int(regCode, base=2)]	#and uses it to get the reg encoded
 
+def intRead(intCode, neg): #intCode is the 3 bits that aren't the pos/flag, neg is pos/neg, both strings
+	val = int(intCode, base=2)
+	if(int(neg, base=2)):
+		val = val *-1
+	return val
